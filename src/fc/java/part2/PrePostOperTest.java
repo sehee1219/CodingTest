932e1c2da00cbf2e287bfc85e9a01b034e07ba9e
Hasehee <118956516+hasehee@users.noreply.github.com>
2024-03-03 17:28:28 +0900

    Stack

---
+++ src/fc/java/part2/PrePostOperTest.java
@@ -0,0 +1,13 @@
+package fc.java.part2;
+
+public class PrePostOperTest {
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(--x);
+        System.out.println(x++);
+        System.out.println(x--);
+        System.out.println(++x);
+        System.out.println(x);
+
+    }
+}
