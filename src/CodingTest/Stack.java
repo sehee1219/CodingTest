28a36902df7824c6538bd3a33d546252d7a89fb8
Hasehee <118956516+Hasehee@users.noreply.github.com>
2024-03-03 17:14:56 +0900

    Stack

---
+++ src/CodingTest/Stack.java
@@ -0,0 +1,93 @@
+//백준 10828번
+
+
+package CodingTest;
+
+import java.util.Scanner;
+public class Stack {
+
+    public static int[] stack;
+    public static int size = 0;
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = in.nextInt();
+
+        stack = new int[N];
+
+        for(int i = 0; i < N; i++) {
+
+            String str = in.next();
+
+            switch (str) {
+
+                case "push":
+                    push(in.nextInt());
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+
+                case "top":
+                    sb.append(top()).append('\n');
+                    break;
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+    public static void push(int item) {
+        stack[size] = item;
+        size++;
+    }
+
+    public static int pop() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            int res = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return res;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if(size == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            return stack[size - 1];
+        }
+    }
+
+}
+
