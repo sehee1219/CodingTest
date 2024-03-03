6cb05ad77ede2530c8d192ec48db3b6a39d1221f
Hasehee <118956516+hasehee@users.noreply.github.com>
2024-03-03 17:29:51 +0900

    //

---
+++ src/fc/java/part2/RelationalOperTest.java
@@ -0,0 +1,13 @@
+package fc.java.part2;
+
+public class RelationalOperTest {
+    public static void main(String[] args) {
+        int a=4;
+        System.out.println(a>5);
+        System.out.println(a<5);
+        System.out.println(a==6);
+        System.out.println(a!=4);
+        System.out.println(3>=a);
+        System.out.println(a<=a+1);
+    }
+}
